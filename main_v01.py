fe0ab5fd1a0fa1b3efc9d4beb71c29e4b2ffc368
Sarvenaz Ch <53100295+sarvenaz-ch@users.noreply.github.com>
2022-11-13 22:40:04 -0800

    kde plot is plotted

--- main_v01.py
+++ main_v01.py
@@ -7,6 +7,11 @@ Elegant Scipy
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('style/elegant.mplstyle')
+from scipy import stats
+
+# %%
 
 def rmkm(counts, length):
     '''
@@ -27,5 +32,61 @@ if __name__ == '__main__':
         
     samples = list(data_table.columns)
     
-    file_genes = fldr_curr+'\data\gene.csv'
+    file_genes = fldr_curr+'\data\genes.csv'
     gene_info = pd.read_csv(file_genes)
+    
+    
+    print(f'loaded the right data? {data_table.shape[0]==gene_info.shape[0]}')
+    
+    # %%
+    # Lets match the two dataset we have
+    gene_info.index = gene_info.GeneSymbol
+    matched_index = pd.Index.intersection(data_table.index, gene_info.index)
+    
+    counts = np.asarray(data_table.loc[matched_index], dtype = int)
+    gene_names = np.array(matched_index)
+    
+    gene_length = np.asarray(gene_info.loc[matched_index]['GeneLength'], dtype = int)
+    
+    total_counts = np.sum(counts, axis = 0)
+    
+    # Use Gaussian smoothing to estimate the density
+    density = stats.kde.gaussian_kde(total_counts)
+    
+    # Make values for ehivh to estimate the density for plotting
+    x = np.arange(min(total_counts), max(total_counts), 10000)
+    
+    # display
+    fig, ax = plt.subplot()
+    ax.plt(x, density(x))
+    ax.set_xlabel('Total counts per individual')
+    ax.set_ylabel('Density')    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
